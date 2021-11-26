94f7dd86a3530571cb5c4f6a2b669259d3de3acd
mustitisnt <74868073+mustitisnt@users.noreply.github.com>
2021-11-26 21:50:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from snake import snake, tail
 from eat import eat
 from score import score
 from function import pause
-import time
 import board
 import busio
 from adafruit_ht16k33 import matrix
@@ -15,8 +14,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 matrix = matrix.Matrix8x8(i2c)
 
 # edges of an 8x8 matrix
-col_max = 8
-row_max = 8
+col_max = 7
+row_max = 7
 
 # Clear the matrix.
 matrix.fill(0)
@@ -48,6 +47,31 @@ tailPiece = []
 tailPiece.append(tail(screen,h, w, snake1.x - snake1.size, snake1.y, 1))
 font = pygame.font.SysFont("Arial", 30, True)
 score1 = score(screen, font)
+"""gleicht die Verschiebung der Matrix aus"""
+def xwechsel(x):
+    """if x == 0:
+        x = 7
+    elif x == 1:
+        x = 6
+    elif x == 2:
+        x = 5
+    elif x == 3:
+        x = 4
+    elif x == 4:
+        x = 3
+    elif x == 5:
+        x = 2
+    elif x == 6:
+        x = 1
+    else:
+        x = 0"""
+    return(x)
+def ywechsel(y):
+    if y == 7:
+        y = 0
+    else:
+        y = y + 1
+    return(y)
 while True:
     screen.fill(black)
     event = pygame.event.get()
@@ -64,14 +88,17 @@ while True:
 
     for eat in eatl:
         eat.draw()
-        xl = (eat.x/50) - 1
-        yl = (eat.y/50) - 1
+        xl = int((eat.x/50))
+        yl = int((eat.y/50))
+        xl = xwechsel(xl)
+        yl = ywechsel(yl)
         """Warscheinlich gibt es verschiebungen mit x und y und so"""
 
         matrix[yl, xl] = 2
 
     score1.draw(snake1.length)
     if snake1.x % snake1.size == 0 and snake1.y % snake1.size == 0:
+        matrix.fill(0)
         x = int(snake1.x / 50)
         y = int(snake1.y / 50)
         for piece in tailPiece:
@@ -83,7 +110,7 @@ while True:
             elif piece.direction == 4:
                 xp = x
             elif piece.direction == 3:
-                xp = xp - 1
+                xp = xp + 1
             elif piece.direction == 1:
                 xp = xp
                 yp = yp
@@ -95,6 +122,8 @@ while True:
                 xp = xp - 1
             elif piece.direction == 4:
                 yp = yp + 1
+            xp = xwechsel(xp)
+            yp = ywechsel(yp)
             try:
                 M[yp][xp] = 1
                 """Warscheinlich gibt es verschiebungen mit x und y und so"""
@@ -102,18 +131,18 @@ while True:
                 matrix[yp, xp] = 2
             except:
                 print("Auserhalb der Reichweite Schwanz")
+
         if snake1.direction == 2:
             x = x
-            y = y - 1
-        if snake1.direction == 1:
+            y = y + 1
+        elif snake1.direction == 1:
             x = x + 1
-        elif snake1.direction == 2:
-            y = y - 1
         elif snake1.direction == 3:
             x = x - 1
         elif snake1.direction == 4:
             y = y + 1
-
+        x = xwechsel(x)
+        y = ywechsel(y)
         try:
             M[y][x] = 1
             """Warscheinlich gibt es verschiebungen mit x und y und so"""
@@ -125,12 +154,12 @@ while True:
         print("\n \n \n \n \n")
         xm = 0
         ym = 0
-        for i in M:
+        """for i in M:
             ym = ym + 1
             for j in i:
                 xm = xm + 1
                 if j == 0:
-                    matrix[ym, xm] = 0
+                    matrix[ym, xm] = 0"""
         M = [[0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0],
@@ -143,5 +172,3 @@ while True:
 
     clock.tick(50) # sollt FPS auf 50 begrenzen
     pygame.display.flip()
-
-
