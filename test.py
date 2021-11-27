7de0692be86e4375cd73ff238fe9bbeb177553aa
mustitisnt <74868073+mustitisnt@users.noreply.github.com>
2021-11-27 20:00:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-""""""
 import pygame
 from snake import snake, tail
 from eat import eat
@@ -15,8 +14,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 matrix = matrix.Matrix8x8(i2c)
 
 # edges of an 8x8 matrix
-col_max = 8
-row_max = 8
+col_max = 7
+row_max = 7
 
 # Clear the matrix.
 matrix.fill(0)
@@ -48,6 +47,16 @@ tailPiece = []
 tailPiece.append(tail(screen,h, w, snake1.x - snake1.size, snake1.y, 1))
 font = pygame.font.SysFont("Arial", 30, True)
 score1 = score(screen, font)
+"""gleicht die Verschiebung der Matrix aus"""
+def xwechsel(x):
+
+    return(x)
+def ywechsel(y):
+    if y == 7:
+        y = 0
+    else:
+        y = y + 1
+    return(y)
 while True:
     screen.fill(black)
     event = pygame.event.get()
@@ -64,24 +73,27 @@ while True:
 
     for eat in eatl:
         eat.draw()
-        xl = (eat.x/50) - 1
-        yl = (eat.y/50) - 1
+        xl = int((eat.x/50))
+        yl = int((eat.y/50))
+        xl = xwechsel(xl)
+        yl = ywechsel(yl)
         """Warscheinlich gibt es verschiebungen mit x und y und so"""
 
         matrix[yl, xl] = 2
 
     score1.draw(snake1.length)
     if snake1.x % snake1.size == 0 and snake1.y % snake1.size == 0:
+        matrix.fill(0)
         x = int(snake1.x / 50)
         y = int(snake1.y / 50)
         for piece in tailPiece:
             xp = int(piece.x/50)
             yp = int(piece.y/50)
             if piece.direction == 2:
-                xp = x
+                xp = xp + 1
                 yp = yp
             elif piece.direction == 4:
-                xp = x
+                xp = xp + 1
             elif piece.direction == 3:
                 xp = xp + 1
             elif piece.direction == 1:
@@ -95,11 +107,8 @@ while True:
                 xp = xp - 1
             elif piece.direction == 4:
                 yp = yp + 1
-            """Verschiebung der Matrix ausgleichen hoffentlich"""
-            if yp == 7:
-                yp = 0
-            else:
-                yp = yp + 1
+            xp = xwechsel(xp)
+            yp = ywechsel(yp)
             try:
                 M[yp][xp] = 1
                 """Warscheinlich gibt es verschiebungen mit x und y und so"""
@@ -110,20 +119,15 @@ while True:
 
         if snake1.direction == 2:
             x = x
-            y = y - 1
-        if snake1.direction == 1:
+            y = y + 1
+        elif snake1.direction == 1:
             x = x + 1
-        elif snake1.direction == 2:
-            y = y - 1
         elif snake1.direction == 3:
             x = x - 1
         elif snake1.direction == 4:
             y = y + 1
-        """Verschiebung der Matrix ausgleichen hoffentlich"""
-        if yp == 7:
-            yp = 0
-        else:
-            yp = yp + 1
+        x = xwechsel(x)
+        y = ywechsel(y)
         try:
             M[y][x] = 1
             """Warscheinlich gibt es verschiebungen mit x und y und so"""
@@ -135,12 +139,12 @@ while True:
         print("\n \n \n \n \n")
         xm = 0
         ym = 0
-        for i in M:
+        """for i in M:
             ym = ym + 1
             for j in i:
                 xm = xm + 1
                 if j == 0:
-                    matrix[ym, xm] = 0
+                    matrix[ym, xm] = 0"""
         M = [[0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0],
      [0, 0, 0, 0, 0, 0, 0, 0],
